398d585f066c1e3e050f79ae83a31abe85f4b89c
arkadiysotnikov <cotnikoarkady@gmail.com>
2023-11-10 07:44:13 +0400

    fix(examples): fix incorrect ids in queries

--- data/examples/1/sql/2_location.sql
+++ data/examples/1/sql/2_location.sql
@@ -1,15 +1,15 @@
 INSERT INTO astute_backend_location
-(id, coordinates_horizontal, coordinates_vertical, name, location_type)
+(id, coordinates_horizontal, coordinates_vertical, name, location_type_id)
 VALUES ('d6f3b4df-b820-46ee-b7bc-2be11dc25f9e', 1, 1, 'Шкаф с шоколадом', 'a7984a3b-9887-4b9f-8217-bc455d5093a8');
 
 INSERT INTO astute_backend_location
-(id, coordinates_horizontal, coordinates_vertical, name, location_type)
+(id, coordinates_horizontal, coordinates_vertical, name, location_type_id)
 VALUES ('3f605d40-e4a0-4017-9316-b568f9250bdf', 2, 1, 'Шкаф с конфетами', 'a7984a3b-9887-4b9f-8217-bc455d5093a8');
 
 INSERT INTO astute_backend_location
-(id, coordinates_horizontal, coordinates_vertical, name, location_type)
+(id, coordinates_horizontal, coordinates_vertical, name, location_type_id)
 VALUES ('502626b1-508e-458c-a90f-e356d1fa15f0', 2, 2, 'Шкаф с сахаром', '31c530ce-f0d3-40bc-9206-85da4c250b78');
 
 INSERT INTO astute_backend_location
-(id, coordinates_horizontal, coordinates_vertical, name, location_type)
-VALUES ('3f605d40-e4a0-4017-9316-b568f9250bdf', 3, 2, 'Шкаф с закусками', '26431182-a713-420c-bff0-b8f9af3b0ef6');
+(id, coordinates_horizontal, coordinates_vertical, name, location_type_id)
+VALUES ('a14cefb7-23c6-4360-a0e5-815c4fede3d0', 3, 2, 'Шкаф с закусками', '26431182-a713-420c-bff0-b8f9af3b0ef6');
