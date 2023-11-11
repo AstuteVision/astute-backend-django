2da24d4a66c643be81c7dd8f1e78b6d8265e547d
arkadiysotnikov <cotnikoarkady@gmail.com>
2023-11-11 04:44:06 +0400

    fix(packages): add missing packages

--- test_websocket.py
+++ test_websocket.py
@@ -6,6 +6,7 @@ import websockets
 async def test_websocket():
     headers = {'client-id': '1'}
     async with websockets.connect("ws://127.0.0.1:8000/ws", extra_headers=headers) as websocket:
+        # here goods uuids to buy
         await websocket.send("['f360b82d-8e49-41d4-adf2-5b409a256dea']")
         while True:
             data = await websocket.recv()
